0c07f42149115f99c22c4f6282f5b472853447fa
Peter <pbd281@gmail.com>
2019-07-14 15:49:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@
 from distutils.core import setup
 
 setup(
-    name='decompress',
+    name='decompress-decrypt',
     version='0.1.0',
-    packages=['decompress',],
+    packages=['decompress_decrypt'],
     install_requires=[
         'boto3==1.9.137',
         'botocore==1.12.137',
         'Click==7.0',
+        'cryptography==2.7',
         'smart-open==1.8.4'
     ],
     entry_points={
         'console_scripts': [
-	        'decompress=decompress:main',
+	        'decompress-decrypt=decompress_decrypt:main',
         ],
     },
 )
